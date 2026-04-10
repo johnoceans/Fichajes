1e2ecc09b1245af8c6e69ad92ff289ef5e0f8921
John <johnooceans@gmail.com>
2026-04-11 00:06:29 +0200

    Ejercicio 9.2.0

---
+++ bin/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"<Unnamed>","l":"App"},{"p":"<Unnamed>","l":"Entrenador"},{"p":"<Unnamed>","l":"Equipo"},{"p":"<Unnamed>","l":"Jugador"}];updateSearchResults();
